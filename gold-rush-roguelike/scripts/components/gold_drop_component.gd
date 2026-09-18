7aaa48606a0f7876d4fe36df902bfad7f5e81d8c
motomotoo1997-art <motomotoo1997@gmail.com>
2026-09-19 02:07:20 +0300

    feat: add gold drop component

---
+++ gold-rush-roguelike/scripts/components/gold_drop_component.gd
@@ -0,0 +1,38 @@
+extends Node
+class_name GoldDropComponent
+
+@export_range(1, 10000, 1) var gold_amount: int = 5
+@export var pickup_scene: PackedScene
+
+
+func _ready() -> void:
+	var owner_node := get_parent()
+	if owner_node == null:
+		return
+	var health := owner_node.get_node_or_null("HealthComponent") as HealthComponent
+	if health != null:
+		health.died.connect(_on_owner_died)
+
+
+func spawn_drop(world_position: Vector2) -> GoldPickup:
+	if pickup_scene == null:
+		return null
+	var pickup := pickup_scene.instantiate() as GoldPickup
+	if pickup == null:
+		return null
+	var target_parent := get_tree().current_scene
+	if target_parent == null:
+		target_parent = get_parent().get_parent()
+	if target_parent == null:
+		pickup.free()
+		return null
+	target_parent.add_child(pickup)
+	pickup.global_position = world_position
+	pickup.configure(gold_amount)
+	return pickup
+
+
+func _on_owner_died() -> void:
+	var owner_node := get_parent() as Node2D
+	if owner_node != null:
+		spawn_drop(owner_node.global_position)
