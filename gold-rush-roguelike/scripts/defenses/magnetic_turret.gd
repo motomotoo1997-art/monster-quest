adb60ce482276d38d91bab24cb0eea36a2cad7ca
motomotoo1997-art <motomotoo1997@gmail.com>
2026-09-19 02:07:37 +0300

    feat: add Magnetic Turret behavior

---
+++ gold-rush-roguelike/scripts/defenses/magnetic_turret.gd
@@ -0,0 +1,23 @@
+extends DefenseBase
+class_name MagneticTurret
+
+signal target_changed(target: Node2D)
+
+var _current_target: Node2D
+
+@onready var targeting_component: TargetingComponent = $TargetingComponent
+@onready var weapon_component: WeaponComponent = $WeaponComponent
+@onready var aim_pivot: Node2D = $AimPivot
+@onready var muzzle: Marker2D = $AimPivot/Muzzle
+
+
+func _physics_process(_delta: float) -> void:
+	var target := targeting_component.get_nearest_hostile(global_position)
+	if target != _current_target:
+		_current_target = target
+		target_changed.emit(target)
+	if target == null:
+		return
+	var direction := muzzle.global_position.direction_to(target.global_position)
+	aim_pivot.rotation = direction.angle()
+	weapon_component.try_fire(muzzle.global_position, direction, team_component.team)
