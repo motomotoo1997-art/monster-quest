e0b64fb89982b16adff5a32de9e3e9e3195eed12
motomotoo1997-art <motomotoo1997@gmail.com>
2026-09-19 02:12:35 +0300

    test: extend run-flow coverage

--- gold-rush-roguelike/tests/test_run_flow.gd
+++ gold-rush-roguelike/tests/test_run_flow.gd
@@ -1,7 +1,15 @@
 extends SceneTree
 
+var _all_arenas_completed := false
+
 
 func _init() -> void:
+	_test_enemy_target_fallback()
+	_test_arena_progression()
+	quit(0)
+
+
+func _test_enemy_target_fallback() -> void:
 	var enemy := EnemyBase.new()
 	enemy.prefer_core_weight = 1.0
 	var player := Node2D.new()
@@ -15,4 +23,43 @@ func _init() -> void:
 	core.free()
 	assert(enemy.choose_target() == null, "Enemy must return null when no valid target remains")
 	enemy.free()
-	quit(0)
+
+
+func _test_arena_progression() -> void:
+	var container := Node2D.new()
+	container.name = "ArenaContainer"
+	root.add_child(container)
+
+	var controller := ArenaController.new()
+	controller.name = "ArenaController"
+	controller.arena_container_path = NodePath("../ArenaContainer")
+	controller.arena_scenes = _make_test_arenas(5)
+	controller.all_arenas_completed.connect(_on_all_arenas_completed)
+	root.add_child(controller)
+
+	for expected_index in range(1, 6):
+		assert(controller.load_arena(expected_index))
+		assert(controller.current_arena_index == expected_index)
+		controller.complete_current_arena()
+
+	assert(_all_arenas_completed, "Completing arena 5 must emit all_arenas_completed")
+	assert(not controller.load_next_arena(), "Arena 6 must not load")
+	assert(controller.current_arena_index == 5)
+	controller.queue_free()
+	container.queue_free()
+
+
+func _make_test_arenas(count: int) -> Array[PackedScene]:
+	var scenes: Array[PackedScene] = []
+	for i in range(count):
+		var node := Node2D.new()
+		node.name = "Arena%d" % (i + 1)
+		var packed := PackedScene.new()
+		assert(packed.pack(node) == OK)
+		node.free()
+		scenes.append(packed)
+	return scenes
+
+
+func _on_all_arenas_completed() -> void:
+	_all_arenas_completed = true
