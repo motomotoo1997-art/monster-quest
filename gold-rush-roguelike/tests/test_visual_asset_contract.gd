363d77082ca46b22ffd155611f165f263ae60fd1
motomotoo1997-art <motomotoo1997@gmail.com>
2026-09-19 23:03:50 +0300

    test: approve Arena02-04 frontier v6 backdrops

--- gold-rush-roguelike/tests/test_visual_asset_contract.gd
+++ gold-rush-roguelike/tests/test_visual_asset_contract.gd
@@ -14,9 +14,9 @@ const EXPECTED := [
 ]
 
 const ARENA_BACKDROPS := [
-	{"scene":"res://scenes/arenas/Arena02.tscn","texture":"arena02_backdrop_v2.svg"},
-	{"scene":"res://scenes/arenas/Arena03.tscn","texture":"arena03_backdrop_v2.svg"},
-	{"scene":"res://scenes/arenas/Arena04.tscn","texture":"arena04_backdrop_v2.svg"},
+	{"scene":"res://scenes/arenas/Arena02.tscn","texture":"arena02_mining_yard_v6.svg"},
+	{"scene":"res://scenes/arenas/Arena03.tscn","texture":"arena03_rail_explosives_v6.svg"},
+	{"scene":"res://scenes/arenas/Arena04.tscn","texture":"arena04_elite_canyon_v6.svg"},
 	{"scene":"res://scenes/arenas/Arena05.tscn","texture":"arena05_backdrop_v2.svg"},
 ]
 
