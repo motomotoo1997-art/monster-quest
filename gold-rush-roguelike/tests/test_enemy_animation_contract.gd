291147a0d2b8337625afdf1a6e9f31b4da1b62d0
motomotoo1997-art <motomotoo1997@gmail.com>
2026-09-19 21:33:14 +0300

    test: validate animations against approved reference atlas

--- gold-rush-roguelike/tests/test_enemy_animation_contract.gd
+++ gold-rush-roguelike/tests/test_enemy_animation_contract.gd
@@ -1,6 +1,6 @@
 extends SceneTree
 
-const ATLAS_PATH := "res://assets/sprites/enemies/enemies_animation_v4.svg"
+const ATLAS_PATH := "res://assets/sprites/enemies/enemies_reference_v5.svg"
 const EXPECTED := [
 	{"scene":"res://scenes/enemies/GoldHopper.tscn","animations":{&"idle":2,&"move":4,&"hop":4}},
 	{"scene":"res://scenes/enemies/GoldCoinSentinel.tscn","animations":{&"idle":3,&"move":3,&"attack":3},"presentation_nodes":["TelegraphCore","EmitterGlow"]},
